921d0aaeb3387ef671a683fdfc012b392501a246
mk <mk@bczx365.com>
2018-10-15 16:12:54 +0800

    first commit the file;

---
+++ evshang-registration/src/main/java/com/evshang/evshangregistration/EvshangRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.evshang.evshangregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvshangRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvshangRegistrationApplication.class, args);
+    }
+}
---
+++ git.md
@@ -0,0 +1,8 @@
+这是我在git提交的时候遇到的一个问题的解决方式
+> git pull origin master –allow-unrelated-histories
+
+> git push -u origin master -f 
+
+
+
+
