343fcabca43fe4d9dbf6c8127d092f1c11d04b3c
mk <mk@bczx365.com>
2018-10-16 14:29:52 +0800

    first add configuration and authentication

---
+++ evshang-authentication/src/main/java/com/example/evshangauthentication/EvshangAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.example.evshangauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvshangAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvshangAuthenticationApplication.class, args);
+    }
+}
---
+++ evshang-configuration/src/main/java/com/example/evshangconfiguration/EvshangConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.example.evshangconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvshangConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvshangConfigurationApplication.class, args);
+    }
+}
