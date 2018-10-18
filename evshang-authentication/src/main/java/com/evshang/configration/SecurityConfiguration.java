1aa50d19184fce065f2522460ce51fb136d7d724
mk <mk@bczx365.com>
2018-10-18 17:20:28 +0800

    add module gateway

---
+++ evshang-authentication/src/main/java/com/evshang/configration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.evshang.configration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
+
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+
+        super.configure(resources);
+    }
+
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
+
